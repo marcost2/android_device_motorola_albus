c66bebe2e8d120386b7f83c87407c0e8142001ad
Jeferson Oliveira <jroliveira.oliveira301@gmail.com>
2021-10-04 22:24:13 +0200

    albus: Squashed removal of TetheringOverlay
    * This was used to disable BPF, which we now support.
    
    This is a squashed commit of:
    
        Revert "nash: Install TetheringOverlay into vendor"
    
        This reverts commit a16e4d3f0cbd07d33db88ae8b16b3d531132cc10.
    
        Change-Id: Idbfc7b6c3d32ecd9f72a2e184dd661c9c281939b
    
        Revert "nash: Sign Tether RROs with default cert"
    
        This reverts commit 7d91ea8526cd5c66a455f0da6205b89895c2ebb3.
    
        Change-Id: I5a87d1c678301517f9d04bca7d89812b005fb312
    
        Revert "nash: TetheringOverlay: Disable BPF"
    
        This reverts commit 8285ea31e4836cd988009907e6b9aec6e3a3674c.
    
        Change-Id: Icdd884e65d3ad9ce2c8e577b4d34a3ae380e48a5
    
    ffk
    
    Change-Id: I067e41645c5c39bd3a60e9462920474d56e2e6f8

--- device.mk
+++ device.mk
@@ -428,9 +428,6 @@ PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
 # Speed profile services and wifi-service to reduce RAM and storage
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 
-# Tethering
-PRODUCT_PACKAGES += \
-    TetheringConfigOverlay
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine-albus.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
