7ca59982bc2c9491b81b88a2c0da60e575509fe0
Georg Veichtlbauer <georg@vware.at>
2023-04-20 23:25:50 +0200

    albus: Kill property for skia reduceOpsTaskSplitting
    This is now obsolete, enabled by default in AOSP as of commit
    108be5bcfad8fdd6474cc88b15f9a21419667189 in frameworks/base.
    
    Change-Id: I96868c259318f597c583ee6e07cf98687d0ef285

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -123,7 +123,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.opengles.version=196610 \
     ro.qualcomm.cabl=0 \
     debug.renderengine.backend=skiaglthreaded \
-    renderthread.skia.reduceopstasksplitting=true \
     vendor.display.disable_scaler=1 \
     ro.hardware.egl=adreno \
     ro.hardware.vulkan=msm8953
